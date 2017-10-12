a544734d67a69692bbac087b164ec993bb4a1079
jffernan <jffernan@comcast.net>
2017-10-11 18:01:03 -0700

    added location input attrs

--- src/App.js
+++ src/App.js
@@ -2,9 +2,12 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
-  fetchData = (event) => {
-    event.preventDefault();
-    console.log('fetch data!');
+  fetchData = (event) => { /* … */ };
+
+  changeLocation = (event) => {
+    this.setState({
+      location: event.target.value
+    });
   };
 
   render() {
@@ -12,8 +15,13 @@ class App extends Component {
       <div>
         <h1>Weather</h1>
         <form onSubmit={this.fetchData}>
-          <label>Please Enter Your Location For:
-            <input placeholder={"City, State, Zip, Country, etc."} type="text" />
+          <label>Please Enter Your Location For Current Weather:
+            <input
+              placeholder={"City, State, Zip, Country, etc."}
+              type="text"
+              value={this.state.location}
+              onChange={this.changeLocation}
+              />
           </label>
         </form>
       </div>
