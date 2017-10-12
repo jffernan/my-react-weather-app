90d637c07dff3b20d6a84b7a7c22c3957c0e0176
jffernan <jffernan@comcast.net>
2017-10-11 20:00:37 -0700

    fixed currentTemp working & styling F degrees

--- src/App.js
+++ src/App.js
@@ -9,15 +9,28 @@ class App extends Component {
 
   fetchData = (event) => {
     event.preventDefault();
+
     var location = encodeURIComponent(this.state.location);
 
     var urlPrefix = 'http://api.openweathermap.org/data/2.5/forecast?q=';
     var urlSuffix = '&APPID=eec418ceb1be72168ff8ff738033e935&units=imperial';
     var url = urlPrefix + location + urlSuffix;
 
+    var self = this;
+
     fetch(url)
+      .then( function(response) { return response; } )
+      .then( function(response) { return response.json(); } )
+      .then( function(data) {
+        self.setState({
+         data: data
+         //temperature: data.main.temp
+        });
+      })
+      /*
       .then(response => response.json())
-      .then(data => self.setState({ data: data.body })); //JSON.parse(data.body)
+      .then(data => self.setState({ data: data.body
+      })); //JSON.parse(data.body)*/
   };
 
   changeLocation = (event) => {
@@ -34,7 +47,7 @@ class App extends Component {
 
     return (
       <div>
-        <h1>Weather</h1>
+        <h1>My React Weather App</h1>
         <form onSubmit={this.fetchData}>
           <label>Please Enter Your Location For Current Weather:
             <input
@@ -47,7 +60,7 @@ class App extends Component {
         </form>
         <p className="temp-wrapper">
           <span className="temp">{ currentTemp }</span>
-          <span className="temp-symbol">°C</span>
+          <span className="temp-symbol">°F</span>
         </p>
       </div>
     );
