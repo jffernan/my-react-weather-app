e290948be29cb7b6f1b349f62d9713eaffd1f7d1
jffernan <jffernan@comcast.net>
2017-10-11 18:12:50 -0700

    coded url vars to fetch weather data

--- src/App.js
+++ src/App.js
@@ -2,7 +2,21 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
-  fetchData = (event) => { /* … */ };
+  state = {
+    location: ''
+  };
+
+  fetchData = (event) => {
+    event.preventDefault();
+    var location = encodeURIComponent(this.state.location);
+
+    var urlPrefix = 'http://api.openweathermap.org/data/2.5/forecast?q=';
+    var urlSuffix = '&APPID=eec418ceb1be72168ff8ff738033e935&units=imperial';
+    var url = urlPrefix + location + urlSuffix;
+
+    
+
+  };
 
   changeLocation = (event) => {
     this.setState({
