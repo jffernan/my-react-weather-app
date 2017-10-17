21052124abc2fafce9a60482b072f9c2ccf668fd
jffernan <jffernan@comcast.net>
2017-10-16 21:24:12 -0700

    test google api

---
+++ src/components/NavBar.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import { NavLink, Link } from 'react-router-dom';
+
+const link = {
+  width: '100px',
+  padding: '12px',
+  margin: '0 6px 6px',
+  background: 'green',
+  textDecoration: 'none',
+  color: 'white',
+  borderRadius: '10px',
+  position: 'center'
+}
+
+const NavBar = () =>
+  <div className="menu">
+    <ul id="nav">
+      <li>
+      <NavLink to="/"
+        activeClassName="active"
+        exact
+        style={link}
+        activeStyle={{
+          background: 'darkblue',
+          fontWeight: 'bold'
+        }}
+      >Home
+      </NavLink>
+      </li>
+      <li>
+      <Link to="/map"
+        activeClassName="active"
+        exact
+        style={link}
+        activeStyle={{
+          background: 'darkblue',
+          fontWeight: 'bold'}}
+      >Map
+      </Link>
+      </li>
+      <li>
+      <Link to="/about"
+        activeClassName="active"
+        exact
+        style={link}
+        activeStyle={{
+          background: 'darkblue',
+          fontWeight: 'bold'
+        }}
+      >About
+      </Link>
+      </li>
+    </ul>
+  </div>
+
+export default NavBar;
