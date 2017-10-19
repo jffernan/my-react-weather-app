4179a4921eced221b6a4187d43aae16cdc97eeb3
jffernan <jffernan@comcast.net>
2017-10-19 10:25:15 -0700

    still testing googlemaps import

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,26 +1,16 @@
 import React from 'react';
 //import { connect } from 'react-redux';
-/*import GoogleMapReact from 'google-map-react';
+//import GoogleMapReact from 'google-map-react';
 
-const AnyReactComponent = ({ text }) => <div>{ text }</div>;
-
-Map.defaultProps = {
-  center: { lat: 40.7446790, lng: -73.9485420 },
-  zoom: 11
-};
-*/
-
-export default class Map extends React.Component {
-  render() {
-    return (
-      <div>
-        <h1 className="text-center page-title">Map Location</h1>
-        <div className="page-text">
-          <p><b>Search for an address on map:</b></p>
-        </div>
+const Map = (props) => {
+  return (
+    <div>
+      <h1 className="text-center page-title">Map Location</h1>
+      <div className="page-text">
+        <p>Search for an address on map:</p>
       </div>
-    );
-  }
-}
+    </div>
+  )
+};
 
-//export default Map;
+export default Map;
