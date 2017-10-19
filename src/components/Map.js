0e0fa75f5219484210663c287f085341b372cd8f
jffernan <jffernan@comcast.net>
2017-10-19 11:37:42 -0700

    Added GoogleMap Link to Map Page

--- src/components/Map.js
+++ src/components/Map.js
@@ -6,9 +6,11 @@ const Map = (props) => {
   return (
     <div>
       <h1 className="text-center page-title">Map Location</h1>
-      <div className="page-text">
-        <p>Search for an address on map:</p>
-      </div>
+        <h4>
+          <a href="https://www.google.com/maps/search/{ props.location}" data-confirm="Are you sure you want to visit an external website?">
+            'Click' To Google Map Your Location
+          </a>
+        </h4>
     </div>
   )
 };
