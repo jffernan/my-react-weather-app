38eab048fcedabbbbe60cba185728df76844ad02
jffernan <jffernan@comcast.net>
2017-10-26 08:47:42 -0700

    turn off autocomplete in form input

--- src/components/LocationForm.js
+++ src/components/LocationForm.js
@@ -16,7 +16,6 @@ const LocationForm = (props) =>
               name="location"
               id="location"
               placeholder={"Type Address, City, State, Zip, or Country."}
-              autocomplete="on"
               value={props.location}
               onChange={props.changeLocationSubmit}
             />
