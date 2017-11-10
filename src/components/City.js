28d6ec1487d98fb7cd24b063becb4d3eac6eea54
jffernan <jffernan@comcast.net>
2017-11-10 11:02:10 -0800

    fixing key={name.id}

--- src/components/City.js
+++ src/components/City.js
@@ -7,21 +7,21 @@ export default class City extends Component {
     this.state = {
       likeCounter: 0
     };
-      this.handleClickLike = this.handleClickLike.bind(this);
   };
 
   handleClick = () => {
     this.props.onClick(this.props.cityName)
   }
+
 //On click, updating total Likes in Rails DB
-  handleClickLike = () => {
+  handleClickLike = (id) => {
     let likeCounter = this.state.likeCounter;
     let self = this;
     let data = {
       total: likeCounter
     }
 
-    fetch(`/api/v1/cities/${this.props.cityName.id}`, {
+    fetch(`/api/v1/cities/${id}`, {
       method: "PUT",
       headers: {
         'Accept': 'application/json',
@@ -44,7 +44,7 @@ export default class City extends Component {
         </li>
         <button
           className = "likeOption"
-          onClick={this.handleClickLike}
+          onClick={this.handleClickLike.bind(this, this.props.cityName.id)}
           >LIKE
         </button>&nbsp;
         <span className = "likesCounter">
